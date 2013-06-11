93e5453bb2599c0a3b4b9538e797ce89f386ccaf
Arend Pijls <arend.pijls@wijs.be>
2013-06-11 17:01:38 +0200

    create is valid date time function

--- backend/modules/module_maker/engine/helper.php
+++ backend/modules/module_maker/engine/helper.php
@@ -98,4 +98,23 @@ class BackendModuleMakerHelper
 		// return
 		return $name;
 	}
+
+	/**
+	 * Check if datatime is valid
+	 *
+	 * @param string  $dateTime
+	 * @return boolean
+	 */
+	public static function isValidDateTime($dateTime)
+	{
+		if(preg_match("/^(\d{4})-(\d{2})-(\d{2}) ([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $dateTime, $matches))
+		{
+			if(checkdate($matches[2], $matches[3], $matches[1]))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
