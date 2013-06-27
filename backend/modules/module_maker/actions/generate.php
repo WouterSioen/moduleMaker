63daf30dbbc5a917b6c2c6dceea93c7cf01001af
woutersioen <wouter.sioen@wijs.be>
2013-06-27 10:24:40 +0200

    add custom twitter name in twitter card

--- backend/modules/module_maker/actions/generate.php
+++ backend/modules/module_maker/actions/generate.php
@@ -330,21 +330,28 @@ class BackendModuleMakerGenerate extends BackendBaseAction
 		BackendModuleMakerGenerator::generateFile(
 			'frontend/actions/index.base.php', $this->variables, $this->frontendPath . 'actions/index.php'
 		);
-
 		BackendModuleMakerGenerator::generateFile(
 			'frontend/layout/templates/index.base.tpl', $this->variables, $this->frontendPath . 'layout/templates/index.tpl'
 		);
+
+		// create twittercard variable if necessary
+		if(array_key_exists('twitter', $this->record))
+		{
+			$this->variables['twitterCard'] = BackendModuleMakerGenerator::generateSnippet(
+				'frontend/actions/snippets/twittercard.base.php', $this->record
+			);
+		}
+		else $this->variables['twitterCard'] = '';
 
 		// generate detail
 		BackendModuleMakerGenerator::generateFile(
 			'frontend/actions/detail.base.php', $this->variables, $this->frontendPath . 'actions/detail.php'
 		);
-
 		BackendModuleMakerGenerator::generateFile(
 			'frontend/layout/templates/detail.base.tpl', $this->variables, $this->frontendPath . 'layout/templates/detail.tpl'
 		);
 
-		unset($this->variables['pageTitle']);
+		unset($this->variables['pageTitle'], $this->variables['twitterCard']);
 	}
 
 
