b1cdb60366d16d42bcb5aa13434ba669e65c848e
woutersioen <wouter.sioen@wijs.be>
2013-06-27 10:25:47 +0200

    don't forget to add an @ before the twitter name

--- backend/modules/module_maker/layout/templates/frontend/actions/snippets/twittercard.base.php
+++ backend/modules/module_maker/layout/templates/frontend/actions/snippets/twittercard.base.php
@@ -3,8 +3,8 @@
 		 * @TODO add the image
 		 * $this->addMetaData(array('property' => 'twitter:image', 'content' => FRONTEND_FILES_URL . ''), true, 'property');
 		 */
-		$this->addMetaData(array('property' => 'twitter:creator', 'content' => '{$twitter}'), true, 'property');
-		$this->addMetaData(array('property' => 'twitter:site', 'content' => '{$twitter}'), true, 'property');
+		$this->addMetaData(array('property' => 'twitter:creator', 'content' => '@{$twitter}'), true, 'property');
+		$this->addMetaData(array('property' => 'twitter:site', 'content' => '@{$twitter}'), true, 'property');
 		$this->header->addMetaData(array('property' => 'twitter:card', 'content' => 'summary'), true, 'property');
 		$this->header->addMetaData(array('property' => 'twitter:url', 'content' => SITE_URL . FrontendNavigation::getURLForBlock('{$underscored_name}', 'detail') . '/' . $this->record['url']), true, 'property');
 		$this->header->addMetaData(array('property' => 'twitter:title', 'content' => $this->record['meta_title']), true, 'property');
