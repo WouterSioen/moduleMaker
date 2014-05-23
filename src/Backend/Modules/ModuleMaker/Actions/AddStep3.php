bc01ee0fce1dc0b4a943efb44ae39939f44c32be
woutersioen <wouter.sioen@wijs.be>
2014-05-23 14:14:54 +0200

    Fix reference to non existing class - Fixes #53

--- src/Backend/Modules/ModuleMaker/Actions/AddStep3.php
+++ src/Backend/Modules/ModuleMaker/Actions/AddStep3.php
@@ -42,10 +42,10 @@ class AddStep3 extends ActionAdd
     {
         // If step 1 isn't entered, redirect back to the first step of the wizard
         $this->record = \SpoonSession::get('module');
-        if(!$this->record || !array_key_exists('title', $this->record)) $this->redirect(BackendModel::createURLForAction('Add'));
+        if(!$this->record || !array_key_exists('title', $this->record)) $this->redirect(Model::createURLForAction('Add'));
 
         // If there are no fields added, redirect back to the second step of the wizard
-        if(!array_key_exists('fields', $this->record) || empty($this->record['fields'])) $this->redirect(BackendModel::createURLForAction('AddStep2'));
+        if(!array_key_exists('fields', $this->record) || empty($this->record['fields'])) $this->redirect(Model::createURLForAction('AddStep2'));
 
         parent::execute();
 
