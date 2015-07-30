0aef22797ed6a7d291d46a594810268559b29f87
Wouter Sioen <wouter.sioen@gmail.com>
2015-07-30 09:00:44 +0200

    Clean up the getUrlForCategory method
    It's cleaner, has less duplicate code, and follows our coding standards

--- src/Backend/Modules/ModuleMaker/Layout/Templates/Backend/Engine/Snippets/GetUrlCategory.base.php
+++ src/Backend/Modules/ModuleMaker/Layout/Templates/Backend/Engine/Snippets/GetUrlCategory.base.php
@@ -13,30 +13,28 @@
 
         // new category
         if ($id === null) {
-            if ((bool) $db->getVar(
+            $urlExists = (bool) $db->getVar(
                 'SELECT 1
                  FROM {$underscored_name}_categories AS i
                  INNER JOIN meta AS m ON i.meta_id = m.id
                  WHERE i.language = ? AND m.url = ?
                  LIMIT 1',
-                array(Language::getWorkingLanguage(), $url))
-            ) {
-                $url = BackendModel::addNumber($url);
-                return self::getURLForCategory($url);
-            }
+                array(Language::getWorkingLanguage(), $url)
+            );
         } else {
-            // current category should be excluded
-            if ((bool) $db->getVar(
+            $urlExists = (bool) $db->getVar(
                 'SELECT 1
                  FROM {$underscored_name}_categories AS i
                  INNER JOIN meta AS m ON i.meta_id = m.id
                  WHERE i.language = ? AND m.url = ? AND i.id != ?
                  LIMIT 1',
-                array(Language::getWorkingLanguage(), $url, $id))
-            ) {
-                $url = BackendModel::addNumber($url);
-                return self::getURLForCategory($url, $id);
-            }
+                array(Language::getWorkingLanguage(), $url, $id)
+            );
+        }
+
+        if ($urlExists) {
+            $url = BackendModel::addNumber($url);
+            return self::getURLForCategory($url, $id);
         }
 
         return $url;
