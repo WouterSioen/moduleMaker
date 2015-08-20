b31009f00a62316f7f2bbdcff067b14b492cfb33
woutersioen <wouter.sioen@gmail.com>
2015-08-20 08:54:19 +0200

    Fix the getCategoryCount method.
    It didn't take language into account.

--- src/Backend/Modules/ModuleMaker/Layout/Templates/Frontend/Engine/Snippets/getCategoryCount.base.php
+++ src/Backend/Modules/ModuleMaker/Layout/Templates/Frontend/Engine/Snippets/getCategoryCount.base.php
@@ -8,9 +8,9 @@
     public static function getCategoryCount($categoryId)
     {
         return (int) FrontendModel::get('database')->getVar(
-            'SELECT COUNT(i.id) AS count
+            'SELECT COUNT(i.id)
              FROM {$underscored_name} AS i
-             WHERE i.category_id = ?',
-            array((int) $categoryId)
+             WHERE i.language = ? AND i.category_id = ?',
+            array(FRONTEND_LANGUAGE, (int) $categoryId)
         );
     }
