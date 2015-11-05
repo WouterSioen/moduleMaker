48e9669070e0fdfba25aa7ce8d7fccd5ede2785c
Wouter Sioen <wouter.sioen@gmail.com>
2015-11-05 12:10:42 +0100

    Use $this->getModule() instead of the name every time

--- src/Backend/Modules/ModuleMaker/Layout/Templates/Backend/Installer/Snippets/Categories.base.php
+++ src/Backend/Modules/ModuleMaker/Layout/Templates/Backend/Installer/Snippets/Categories.base.php
@@ -1,9 +1,9 @@
 
-        $this->setActionRights(1, '{$camel_case_name}', 'Categories');
-        $this->setActionRights(1, '{$camel_case_name}', 'AddCategory');
-        $this->setActionRights(1, '{$camel_case_name}', 'EditCategory');
-        $this->setActionRights(1, '{$camel_case_name}', 'DeleteCategory');
-        $this->setActionRights(1, '{$camel_case_name}', 'SequenceCategories');
+        $this->setActionRights(1, $this->getModule(), 'Categories');
+        $this->setActionRights(1, $this->getModule(), 'AddCategory');
+        $this->setActionRights(1, $this->getModule(), 'EditCategory');
+        $this->setActionRights(1, $this->getModule(), 'DeleteCategory');
+        $this->setActionRights(1, $this->getModule(), 'SequenceCategories');
 
-        $this->insertExtra('{$camel_case_name}', 'block', '{$camel_case_name}Category', 'Category', null, 'N', 1002);
-        $this->insertExtra('{$camel_case_name}', 'widget', 'Categories', 'Categories', null, 'N', 1003);
+        $this->insertExtra($this->getModule(), 'block', '{$camel_case_name}Category', 'Category', null, 'N', 1002);
+        $this->insertExtra($this->getModule(), 'widget', 'Categories', 'Categories', null, 'N', 1003);
